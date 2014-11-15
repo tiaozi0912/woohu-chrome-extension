21942cef3188e5f04b4d40e7c84128554b3aa6eb
Yujun Wu <wyj0912@gmail.com>
2014-11-14 22:59:50 -0800

    Implement choose and re-choose

--- content.js
+++ content.js
@@ -1,6 +1,8 @@
 (function() {
   'use strict';
 
+  var chooser;
+
   /* Listen for messages */
   chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     /* If the received message has the expected format... */
@@ -9,9 +11,13 @@
     //      the web-pages DOM content as argument 
     //   sendResponse(document.all[0].outerHTML);
     // }
-    var chooser = new window.Woohu.Chooser(msg.template);
+    chooser = chooser || new window.Woohu.Chooser(msg.template);
 
-    chooser.view.render();
+    if (chooser.$el) {
+      chooser.view.update();
+    } else { 
+      chooser.view.render();
+    }
     
     //var inspector = new window.Woohu.Inspector();
   });
--- woohu-chooser.js
+++ woohu-chooser.js
@@ -22,16 +22,16 @@
 
   window.Woohu = window.Woohu || {};
 
-  function _delegateEvents(events, flag) {
+  function _delegateEvents(events, flag, context) {
     _.each(events, function(handler, key) {
       var parsedKey = key.split(' '),
           action = parsedKey[0],
           selector = parsedKey[1];
 
       if (flag) {
-        $('body').on(action, selector, handler);
+        $('body').on(action, selector, handler.bind(context));
       } else {
-        $('body').off(action, selector, handler);
+        $('body').off(action, selector);
       }
     });
   };
@@ -39,8 +39,12 @@
   /**
    * Inspect element by mouse position.
    * Perform actions on the inspected element - highlight, unhighlight, select, unselect
+   * 
+   * @param {Dict} options 
+   * -- skip: the {Element} node that the inspector skips
+   * -- onSelectElement: call back on the element selected
    */
-  var Inspector = function() {
+  var Inspector = function(options) {
     /**
      * Define inspector model
      */
@@ -51,6 +55,9 @@
 
     var _this = this,
         model = this.model;
+
+    this.options = options;
+    this.element = null;
     
     /**
      * Define inspector view. Call view.render() to render(update) the view
@@ -89,24 +96,23 @@
       events: {
         // Dismiss the selection and re-select
         'click .woohu-btn-reselect': function(e) {
-          model.selectedElement = null;
+          model.selectedElement = model.highlightedElement = null;
           view.update();
         },
         // Select an element
         'click .woohu-highlight': function(e) {
           model.selectedElement = model.highlightedElement;
           view.update();
-          console.log('selected element:');
-          console.log(model.selectedElement);
+          if (options.onSelectElement) {
+            options.onSelectElement(model.selectedElement);
+          }
         }
       }
     };
     
     var view = this.view;
 
-    this.element = null;
-
-    _delegateEvents(view.events, true);
+    _delegateEvents(view.events, true, this);
 
     $(document).on('woohu:mousemovestop', function(e, mouseEvent) {
       if (!model.selectedElement) {
@@ -122,6 +128,13 @@
       console.log('element not found;');
       return false;
     }
+
+    if (this.options.skip) {
+      var node = this.options.skip;
+      if (this._in(node, element)) {
+        return false;
+      }
+    }
     
     // set / update highlightedElement
     this.model.highlightedElement = element;
@@ -135,14 +148,37 @@
     return this.element;
   };
 
-  Inspector.prototype.destroy = function() {
-    $('.woohu-inspector').remove();
-    _delegateEvents(this.view.events, false);
+  Inspector.prototype.setSelectedElement = function(element) {
+    this.model.selectedElement = this.model.highlightedElement = element;
+    this.view.update();
   };
 
+  Inspector.prototype.destroy = function() {
+    $('.woohu-inspector').remove();
+    $(document).off('woohu:mousemovestop');
+    _delegateEvents(this.view.events, false);
+  };
+  
+  /**
+   * Check if node is one of the children or sub-children node of parentNode
+   */
+  Inspector.prototype._in = function(parentNode, node) {
+    if (parentNode === node.parentNode) {
+      return true;
+    } else if (node === $('body')[0]) {
+      return false;
+    } else {
+      return this._in(parentNode, node.parentNode);
+    }
+  };
+  
+  /**
+   * Define Chooser
+   */
   var Chooser = function(template) {
     template = template.replace(/&lt;/g, '<');
     template = template.replace(/&gt;/g, '>');
+
     this.settings = {
       steps: [
         {
@@ -168,8 +204,17 @@
       step: this.settings.steps[0]
     };
 
+    this.struct = {
+      selector: '',
+      itemSchema: {
+        price: {selector: ''},
+        name: {selector: ''}
+      }
+    };
+
     var _this = this,
-        model = this.model;
+        model = this.model,
+        inspector;
 
     this.view = {
       template: '<div id="woohu-chooser"></div>',
@@ -177,26 +222,68 @@
         var view = this,
             $el, compiledTemplate;
          
-        compiledTemplate = _.template(template)({step: model.step});
+        compiledTemplate = _.template(template)({
+          step: model.step,
+          progress: (model.step.index / _this.settings.steps.length) * 100
+        });
 
         $el = $(view.template).html(compiledTemplate);
         $('body').append($el);
 
+        _this.$el = $el;
+
+        if (model.step.index < 4) {
+          _this.inspector = new Inspector({
+            onSelectElement: _this.selectElement.bind(_this),
+            skip: $el[0]
+          });
+        }
+
         return $el;
       },
       update: function() {
-
+        _this.$el.remove();
+        if (_this.inspector) {
+          _this.inspector.destroy();
+        }
+        console.log(_this.struct);
+        _this.$el = this.render();
       },
       events: {
         'click .woohu-icon-back': function(e) {
-          console.log('clicked');
+          var currStep = _this.model.step.index;
+
+          _this.model.step = _this.settings.steps[currStep - 2];
+          _this.view.update();
+
+          _this.inspector.setSelectedElement(_this.selectedElement);          
         }
       }
     };
 
-    _delegateEvents(this.view.events, true);
+    _delegateEvents(this.view.events, true, this);
   };
-  
+
+  Chooser.prototype.selectElement = function(selectedElement) {
+    var model = this.model,
+        struct = this.struct,
+        currStep = model.step.index,
+        selector = $(selectedElement).attr('class');
+
+    this.selectedElement = selectedElement;
+
+    if (currStep === 1) {
+      struct.selector = selector;
+    } else if (currStep === 2) {
+      struct.itemSchema.name.selector = selector;
+    } else if (currStep === 3) {
+      struct.itemSchema.price.selector = selector;
+    }
+
+    model.step = this.settings.steps[currStep];
+    this.view.update();
+  };
+
   window.Woohu.Inspector = Inspector;
   window.Woohu.Chooser = Chooser;
 
